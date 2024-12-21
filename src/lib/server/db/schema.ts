6464a3329c9ce6ee3207c10a9bc0133295c36250
Claude White <claude.white2005@gmail.com>
2024-12-21 16:13:36 -0500

    Changed schema

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -15,6 +15,33 @@ export const session = sqliteTable('session', {
 	expiresAt: integer('expires_at', { mode: 'timestamp' }).notNull()
 });
 
-export type Session = typeof session.$inferSelect;
+export const documentation = sqliteTable('documentation', {
+	id: text('id').primaryKey(),
+	userId: text('user_id')
+		.notNull()
+		.references(() => user.id, { onDelete: 'cascade' }),
+	name: text('name').notNull()
+});
 
+export const pageGroup = sqliteTable('page_group', {
+	id: text('id').primaryKey(),
+	documentationId: text('documentation_id')
+		.notNull()
+		.references(() => documentation.id, { onDelete: 'cascade' }),
+	name: text('name').notNull()
+});
+
+export const page = sqliteTable('page', {
+	id: text('id').primaryKey(),
+	pageGroupId: text('page_group_id')
+		.notNull()
+		.references(() => pageGroup.id, { onDelete: 'cascade' }),
+	name: text('name').notNull(),
+	markdownContent: text('markdown_content')
+});
+
+export type Session = typeof session.$inferSelect;
 export type User = typeof user.$inferSelect;
+export type Documentation = typeof documentation.$inferSelect;
+export type PageGroup = typeof pageGroup.$inferSelect;
+export type Page = typeof page.$inferSelect;
